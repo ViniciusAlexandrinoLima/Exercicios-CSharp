e473c119f089f32bc681be8914747be4993a8e03
Vinicius <viniciusalexandrino@hotmail.com>
2022-02-08 15:49:43 -0330

    Mais Exercicios

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,29 @@
+﻿//16 -  Ler 4 números inteiros e calcular a soma dos que forem par.
+namespace Exercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            SomaNumeroInteiro();
+        }
+
+        static void SomaNumeroInteiro()
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Digite 4 numeros");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                if(numero % 2 == 0)
+                {
+                    int soma = numero + numero;
+                    Console.WriteLine($"O numero digitado é par, então a soma é: {soma}");
+                } else
+                {
+                    Console.WriteLine("O numero digitado não é par.");
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio17/Program.cs
@@ -0,0 +1,43 @@
+﻿// 17 - Ler três valores e determinar o maior dentre eles.
+namespace Exercicio17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            MaiorValorEntreTres();
+            
+        }
+
+    static void MaiorValorEntreTres()
+    {
+        Console.WriteLine("Digite um valor");
+        int valor1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite outro valor");
+        int valor2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite mais um valor");
+        int valor3 = Convert.ToInt32(Console.ReadLine());
+
+        if(valor1 > valor2 && valor1 > valor3)
+        {
+            Console.WriteLine("O primeiro valor é maior!");
+        }
+        else if(valor2 > valor1 && valor2 > valor3)
+        {
+            Console.WriteLine("O segundo valor é maior!");
+        }
+        else if(valor3 > valor1 && valor3 > valor2)
+        {
+            Console.WriteLine("O terceiro valor é maior!");
+        }
+        else 
+        {
+            Console.WriteLine("Os valores são iguais");
+        }
+    }
+
+    }
+}
